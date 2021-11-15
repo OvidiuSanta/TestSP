3d72e300420ff2b1d6c729bbeb97908a3a610931
OvidiuSanta <73822689+OvidiuSanta@users.noreply.github.com>
2021-11-15 21:28:46 +0200

    Update Main.java
    am scris clasa Melodii cu m mic in secventa de cod "MyPlaylist.addPlaylistElement("www.google.com/Melodii/oscar-scuze/32");"

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Playlist MyPlaylist = new Playlist("Favorite");
         Videoclip video1 = new Videoclip("Stana Cartoons", 2048);
         MyPlaylist.add(video1);
-        MyPlaylist.addPlaylistElement("www.google.com/melodii/oscar-scuze/32");
+        MyPlaylist.addPlaylistElement("www.google.com/Melodii/oscar-scuze/32");
         MyPlaylist.addPlaylistElement("www.google.com/GIF/Party/10");
 
         MyPlayer.addElement(MyPlaylist);
